ee65785214a8dbec64de176d328f8e1c4f5fac80
pods13 <alta0815@yandex.ru>
2019-09-22 13:31:06 +0300

    Added <Pet name=Doink animal=Cat breed=Mixed />

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
 	return (
 		<div>
 			<h1>Adopt me!</h1>
-			<Pet name="Luna" animal="Dog" breed="Havenese" />
-			<Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-			<Pet name="Doink" animal="Cat" breed="Mixed" />
+			<SearchParams />
 		</div>
 	);
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function SearchParams() {
+	const location = "Seattle, WA";
+
+	return (
+		<div className="search-params">
+			<form>
+				<label htmlFor="location">
+					Location:
+					<input id="location" value={location} placeholder="Location" />
+				</label>
+				<button>Submit</button>
+			</form>
+		</div>
+	);
+}
