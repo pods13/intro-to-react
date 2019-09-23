1ede1267cc5a09464f549778646fbd1297048321
pods13 <alta0815@yandex.ru>
2019-09-23 17:22:45 +0300

    UseEffect to receive data from the api & update data on the page

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 export default function SearchParams() {
 	const [location, setLocation] = useState("Seattle, WA");
+	const [breeds, setBreeds] = useState([]);
 	const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-	const [breed, BreedDropdown] = useDropdown("Breed", "", []);
+	const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+	useEffect(() => {
+		setBreeds([]);
+		setBreed("");
+
+		pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+			const breedNames = apiBreeds.map(({ name }) => name);
+			setBreeds(breedNames);
+		}, console.error);
+	}, [animal, setBreeds, setBreed]);
 
 	return (
 		<div className="search-params">
