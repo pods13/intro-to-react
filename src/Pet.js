34ecb9a490fc92f27a668fd00597e7e91cb538cd
pods13 <alta0815@yandex.ru>
2019-09-25 16:51:45 +0300

    Show results of the search

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-export default function Pet({ name, animal, breed }) {
+export default function Pet({ name, animal, breed, location, id, media }) {
 	return (
-		<div>
-			<h1>{name}</h1>
-			<h2>{animal}</h2>
-			<h2>{breed}</h2>
-		</div>
+		<a href={`/details/${id}`} className="pet">
+			<div className="image-container">
+				<img src={getHeroImage(media)} alt={name} />
+			</div>
+			<div className="info">
+				<h2>{name}</h2>
+				<h2>{`${animal} - ${breed} - ${location}`}</h2>
+			</div>
+		</a>
 	);
 }
+
+const getHeroImage = media => {
+	return media.length ? media[0].small : `http://placecorgi.com/300/300`;
+};
--- src/Results.js
+++ src/Results.js
@@ -3,20 +3,24 @@ import Pet from "./Pet";
 
 export default function Results({ pets }) {
 	return (
-		<div className="">
-			{pets.map(pet => {
-				const petAddress = pet.contact.address;
-				return (
-					<Pet
-						key={pet.id}
-						name={pet.name}
-						animal={pet.animal}
-						breed={pet.breeds.primary}
-						media={pet.photos}
-						location={`${petAddress.city}, ${petAddress.state}`}
-					/>
-				);
-			})}
+		<div className="search">
+			{pets.length == 0 ? (
+				<h1>No pets found</h1>
+			) : (
+				pets.map(pet => {
+					const petAddress = pet.contact.address;
+					return (
+						<Pet
+							key={pet.id}
+							name={pet.name}
+							animal={pet.type}
+							breed={pet.breeds.primary}
+							media={pet.photos}
+							location={`${petAddress.city}, ${petAddress.state}`}
+						/>
+					);
+				})
+			)}
 		</div>
 	);
 }
